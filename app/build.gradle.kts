49432be1cf58a81d42254d0ec5fb1be2d487ef71
3qe5htsgf <marco.kathriner@gmail.com>
2026-08-29 17:50:43 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,13 +6,13 @@ android {
     namespace = "ch.ninebot.f2lab"
     compileSdk = 35
 
-    defaultConfig {
-        applicationId = "ch.ninebot.f2lab"
-        minSdk = 26
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
-    }
+   defaultConfig {
+    applicationId = "ch.ninebot.f2lab.pixel"
+    minSdk = 26
+    targetSdk = 35
+    versionCode = 2
+    versionName = "1.0.1"
+}
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
